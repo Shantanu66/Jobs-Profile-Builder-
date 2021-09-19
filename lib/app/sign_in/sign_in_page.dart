5c7859ce842f62874467859d2c5e04e4cb22da2a
Maxman <shantanur66@gmail.com>
2021-09-20 00:49:31 +0530

    Text added 'Sign In' Shadows added textalign style for font size,font weight,colors,shadows added

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -14,27 +14,37 @@ class SignInPage extends StatelessWidget {
   Widget _BuildContent() {
     return Container(
       color: Colors.deepPurple,
+      padding: EdgeInsets.all(15.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: Colors.pink,
-            child: SizedBox(
-              height: 100.0,
+          Text('Sign In',
+            textAlign: TextAlign.center,
+            style:TextStyle(fontSize: 32.0,fontWeight: FontWeight.w700,
+                color: Colors.white,
+                shadows: [Shadow(
+                offset: Offset(4.0, 4.0),
+                blurRadius: 20.0,
+                color: Color.fromARGB(150, 0, 0, 0),
+            )],
             ),
           ),
+          SizedBox(height: 8.0,),
           Container(
             color: Colors.amber,
             child: SizedBox(
               height: 100.0,
             ),
           ),
+          SizedBox(height: 8.0,),
           Container(
             color: Colors.green,
             child: SizedBox(
               height: 100.0,
             ),
-          )
+          ),
+          SizedBox(height: 8.0,),
         ],
       ),
     );
