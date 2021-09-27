25eb358742c11e06adef70f4e22af06dc3d8a3d0
Maxman <shantanur66@gmail.com>
2021-09-27 23:59:42 +0530

    Dialog Box changed

--- lib/app/sign_in/sign_in_with_email_form.dart
+++ lib/app/sign_in/sign_in_with_email_form.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_button/animated_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/validators.dart';
+import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 enum EmailSignInFormType { signIn, register }
@@ -43,135 +44,123 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       }
       Navigator.of(context).pop();
     } catch (e) {
-      if (Platform.isIOS) {
-        print('show CupertinoAlertDialog');
-      }
-      else {
-        showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text('Sign In Failed'),
-                content: Text(e.toString()),
-                actions:
-                [TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: Text('OK')
-                )
-                ],
-              );
-            });
-      } finally {
+      showAlertDialog(
+        context,
+        title: 'Sign In Failed',
+        content: e.toString(),
+        defaultActionText: 'OK',
+      );
+    } finally {
       setState(() {
         _isLoading = false;
       });
     }
   }
 
-    void _toggleFormType() {
+  void _toggleFormType() {
     setState(() {
-    _formType = _formType == EmailSignInFormType.signIn
-    ? EmailSignInFormType.register
-        : EmailSignInFormType.signIn;
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
     });
     _emailController.clear();
     _passwordController.clear();
-    }
+  }
 
-    List<Widget> _buildChildren() {
+  List<Widget> _buildChildren() {
     final primarytext =
-    _formType == EmailSignInFormType.signIn ? 'Sign In' : 'Create Acc';
+        _formType == EmailSignInFormType.signIn ? 'Sign In' : 'Create Acc';
     final secondarytext = _formType == EmailSignInFormType.signIn
-    ? 'Need an Account?Register'
+        ? 'Need an Account?Register'
         : 'Have an Account?Sign In';
 
     bool submitEnabled = widget.emailValidator.isValid(_email) &&
-    widget.passwordValidator.isValid(_password) &&
-    !_isLoading;
+        widget.passwordValidator.isValid(_password) &&
+        !_isLoading;
     return [
-    _buildEmailTextField(),
-    SizedBox(
-    height: 12.0,
-    ),
-    _buildPasswordTextField(),
-    SizedBox(
-    height: 30.0,
-    ),
-    AnimatedButton(
-    child: Center(
-    child: Text(
-    primarytext,
-    style: TextStyle(
-    color: Colors.white,
-    fontWeight: FontWeight.bold,
-    ),
-    ),
-    ),
-    color: Colors.deepPurple,
-    shadowDegree: ShadowDegree.light,
-    height: 43,
-    width: 110,
-    onPressed: submitEnabled ? _submit : null,
-    enabled: _isLoading == false,
-    ),
-    SizedBox(
-    height: 5.0,
-    ),
-    TextButton(
-    child: Text(
-    secondarytext,
-    style: TextStyle(
-    fontSize: 14,
-    letterSpacing: 1,
-    fontWeight: FontWeight.w500,
-    color: Colors.black87),
-    ),
-    style: ButtonStyle(
-    overlayColor: MaterialStateProperty.all(Colors.transparent)),
-    onPressed: !_isLoading ? _toggleFormType : null,
-    )
+      _buildEmailTextField(),
+      SizedBox(
+        height: 12.0,
+      ),
+      _buildPasswordTextField(),
+      SizedBox(
+        height: 30.0,
+      ),
+      AnimatedButton(
+        child: Center(
+          child: Text(
+            primarytext,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        color: Colors.deepPurple,
+        shadowDegree: ShadowDegree.light,
+        height: 43,
+        width: 110,
+        onPressed: submitEnabled ? _submit : null,
+        enabled: _isLoading == false,
+      ),
+      SizedBox(
+        height: 5.0,
+      ),
+      TextButton(
+        child: Text(
+          secondarytext,
+          style: TextStyle(
+              fontSize: 14,
+              letterSpacing: 1,
+              fontWeight: FontWeight.w500,
+              color: Colors.black87),
+        ),
+        style: ButtonStyle(
+            overlayColor: MaterialStateProperty.all(Colors.transparent)),
+        onPressed: !_isLoading ? _toggleFormType : null,
+      )
     ];
-    }
+  }
 
-    TextField _buildPasswordTextField() {
+  TextField _buildPasswordTextField() {
     bool showerrortext =
-    _submitted && !widget.passwordValidator.isValid(_password);
+        _submitted && !widget.passwordValidator.isValid(_password);
     return TextField(
-    controller: _passwordController,
-    decoration: InputDecoration(
-    labelText: 'Password',
-    errorText: showerrortext ? widget.InvalidPasswordErrorText : null,
-    enabled: _isLoading == false,
-    ),
-    obscureText: true,
-    textInputAction: TextInputAction.done,
+      controller: _passwordController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        errorText: showerrortext ? widget.InvalidPasswordErrorText : null,
+        enabled: _isLoading == false,
+      ),
+      obscureText: true,
+      textInputAction: TextInputAction.done,
     );
-    }
+  }
 
-    TextField _buildEmailTextField() {
+  TextField _buildEmailTextField() {
     bool showErrortext = _submitted && !widget.emailValidator.isValid(_email);
     return TextField(
-    controller: _emailController,
-    decoration: InputDecoration(
-    labelText: 'Email',
-    hintText: 'test@test.com',
-    errorText: showErrortext ? widget.InvalidEmailErrorText : null,
-    enabled: _isLoading == false,
-    ),
-    autocorrect: false,
-    keyboardType: TextInputType.emailAddress,
-    textInputAction: TextInputAction.next,
+      controller: _emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'test@test.com',
+        errorText: showErrortext ? widget.InvalidEmailErrorText : null,
+        enabled: _isLoading == false,
+      ),
+      autocorrect: false,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
     );
-    }
-
-    @override
-    Widget build(BuildContext context) {
-    return Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Column(
-    mainAxisSize: MainAxisSize.min,
-    children: _buildChildren(),
-    ),
-    );
-    }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
+  }
+}
